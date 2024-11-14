2bfcc593cd5cafe8c38e2b6795835093a5a012e5
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-11-14 09:29:32 +0700

    feat: add endpoint to create answer for question by questionId

--- routes/answers.mjs
+++ routes/answers.mjs
@@ -22,4 +22,34 @@ answerRoute.get("/:questionId/answers", async (req, res) => {
   }
 });
 
+answerRoute.post("/:questionId/answers", async (req, res) => {
+  const questionIdFromClient = req.params.questionId;
+  const newAnswer = req.body;
+
+  try {
+    const questionCheck = await connectionPool.query(
+      `SELECT * FROM questions 
+          WHERE id = $1`,
+      [questionIdFromClient]
+    );
+
+    if (!questionCheck.rows[0]) {
+      return res.status(404).json({
+        message: "Question not found.",
+      });
+    }
+
+    await connectionPool.query(
+      `INSERT INTO answers(question_id, content)
+        values($1, $2)`,
+      [questionIdFromClient, newAnswer.content]
+    );
+    return res.status(201).json({
+      message: `Answer created successfully.`,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: `Unable to create answers.` });
+  }
+});
+
 export default answerRoute;
